f9b626672db2012262456d7eac08156bd92dd53f
PotOfCoffee2Go <kim@potofcoffee2go.net>
2017-09-28 19:51:54 +0000

    Moved configuration parameters to front of server.js

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 'use strict'
-/* Runtime uses the current working dir as root - so */
-//  set current working directory to project root
-process.chdir(__dirname);
-
 const
     http = require('http'),
     path = require('path'),
     express = require("express"),
     bodyParser  = require('body-parser'),
     RED = require('node-red');
+
+const config = {
+    port: 8081,
+    homePage: '/posts/6',
+    // Node-RED settings
+    settings: {
+        httpAdminRoot:"/red",     // node-RED flow editor 
+        httpNodeRoot: "/",        // node 'http in' root directory
+        functionGlobalContext: {  // enable function nodes to reference our modules/objects
+            db: require('./db/database') // Blog data store
+        },
+        userDir: path.resolve(__dirname, "node-red"), // Flow storage
+        nodesDir: path.resolve(__dirname, "node-red/nodes"), // Custom nodes
+        flowFilePretty: true,
+        flowFile: 'flows.json',
+        }
+};
+
+/* In some cases the runtime uses the current working dir as root - so */
+//  since server.js is in the project root directory
+//  set current working directory to project root
+process.chdir(__dirname);
 
 /* Express Server */
 // Create an Express app
@@ -21,36 +39,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 /* Routes */
-// Can pick a post as a default page
-app.get('/', function(req, res, next) {
-  req.url = '/posts/8';
-  next();
-});
+// Use a post as the default page
+app.get('/', function(req, res, next) {req.url = config.homePage ? config.homePage : '/'; next();});
 
-// Add a route for static content(css, js, etc) served from 'public' directory
+// Serve static content(css, js, etc) from site root ('public' directory)
 app.use("/",express.static("public"));
 
 /* Node-RED */
-// Create the node-RED settings object
-var settings = {
-    httpAdminRoot:"/red",     // node-RED flow editor 
-    httpNodeRoot: "/",        // node 'http in' root directory
-    functionGlobalContext: {  // enable function nodes to reference our modules/objects
-        db: require('./db/database') // Blog data store
-    },
-    userDir: path.resolve(__dirname, "node-red"), // Flow storage
-    nodesDir: path.resolve(__dirname, "node-red/nodes"), // Custom nodes
-    flowFilePretty: true,
-    flowFile: 'flows.json',
-};
-
-/* Startup */
-// Initialize node-RED runtime with the server and settings
-RED.init(server, settings);
+// Initialize node-RED runtime
+RED.init(server, config.settings);
 // Serve the node-RED Editor and http-in node UIs
-app.use(settings.httpAdminRoot, RED.httpAdmin);
-app.use(settings.httpNodeRoot, RED.httpNode);
-// Fire up the server
-server.listen(8081);
+app.use(config.settings.httpAdminRoot, RED.httpAdmin);
+app.use(config.settings.httpNodeRoot, RED.httpNode);
+// Fire up our server
+server.listen(config.port ? config.port : 8081);
 // Start node-RED runtime
 RED.start();
